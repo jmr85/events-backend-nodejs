7467dfab265d8d201e9b811040374a75963f0f69
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-08-17 06:04:11 -0300

    add .env

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 4000;
+//var port = 4000;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://jruiz-mongodbcloud-back-events:dlE5aEJiXSsMU1zn@cluster0.tvbub.mongodb.net/db_events', { useNewUrlParser: true })
+mongoose.connect(process.env.DB_CNN, { useNewUrlParser: true })
     .then(() => {
         console.log('La conexion a la base de datos correcta!!!!');
         //crear  servidor y ponerme a escuchar peticiones HTTP
-        app.listen(port, () => {
+        app.listen(process.env.PORT, () => {
             console.log(`Servidor corriendo en http://localhost:${port}`);
         });//le pasamos dos param, 1 el puerto y el 2do un callback  para que dentro se ejecute lo quiere hacer en este caso un conole.log()
     });
