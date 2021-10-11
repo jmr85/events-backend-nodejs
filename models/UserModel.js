ea1fb965329f0ef0c0e5351622556f9aa14971e3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-10-11 19:21:00 -0300

    User Schema returnedObject._id.toString()

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,10 +12,9 @@ const UsuarioSchema = Schema({
 
 UsuarioSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id
     delete returnedObject.__v
-    delete returnedObject.passwordHash
   }
 })
 
