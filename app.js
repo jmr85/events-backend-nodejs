d01a23ea64a8fe7d70d4f75d8b17b80c9afcc722
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-10-11 18:57:49 -0300

    vuelvo a dejar el cors como estaba

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 "use strict";
 //1. Cargar modulos de node para crear el servidor.
 const express = require("express");
-const cors = require('cors')
 //2. Ejecutar express (http)
 const app = express();
 //3. Cargar las rutas
 const user_routes = require("./routes/UserRoute");
 
 //4. Middlewares
-app.use(cors());//5. Activar el CORS para permitir peticiones desde el frontend.
 app.use(express.json()); //otro middleware convertir mediante parser cualquier tipo de peticion a json
 app.use(express.urlencoded({ extended: false }));
+
+//5. Activar el CORS para permitir peticiones desde el frontend.
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  next();
+});
 
 //6. Añadir prefijos a las rutas / Cargar rutas
 //app.use("/api", user_routes);
